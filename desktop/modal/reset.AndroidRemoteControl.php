6099e771276067471b607dca8a8b0e896124591f
Halbout Michael <michaelhalbout@gmail.com>
2020-10-22 12:03:59 +0200

    Update reset.AndroidRemoteControl.php

--- desktop/modal/reset.AndroidRemoteControl.php
+++ desktop/modal/reset.AndroidRemoteControl.php
@@ -3,31 +3,31 @@ if (!isConnect('admin')) {
 	throw new Exception('{{401 - Accès non autorisé}}');
 }
 ?>
-<div id='div_resetAndroidRemoteControlAlert' style="display: none;"></div>
-<a class="btn btn-warning pull-right" data-state="1" id="bt_AndroidRemoteControlLogStopStart"><i class="fa fa-pause"></i> {{Pause}}</a>
-<input class="form-control pull-right" id="in_AndroidRemoteControlLogSearch" style="width : 300px;" placeholder="{{Rechercher}}" />
+<div id='div_resetAndroidTVAlert' style="display: none;"></div>
+<a class="btn btn-warning pull-right" data-state="1" id="bt_AndroidTVLogStopStart"><i class="fa fa-pause"></i> {{Pause}}</a>
+<input class="form-control pull-right" id="in_AndroidTVLogSearch" style="width : 300px;" placeholder="{{Rechercher}}" />
 <br/><br/><br/>
-<pre id='pre_AndroidRemoteControlreset' style='overflow: auto; height: 90%;with:90%;'></pre>
+<pre id='pre_AndroidTVreset' style='overflow: auto; height: 90%;with:90%;'></pre>
 
 
 <script>
 	$.ajax({
 		type: 'POST',
-		url: 'plugins/AndroidRemoteControl/core/ajax/AndroidRemoteControl.ajax.php',
+		url: 'plugins/AndroidTV/core/ajax/AndroidTV.ajax.php',
 		data: {
 			action: 'reset'
 		},
 		dataType: 'json',
 		global: false,
 		error: function (request, status, error) {
-			handleAjaxError(request, status, error, $('#div_resetAndroidRemoteControlAlert'));
+			handleAjaxError(request, status, error, $('#div_resetAndroidTVAlert'));
 		},
 		success: function () {
 			 jeedom.log.autoupdate({
-			       log : 'AndroidRemoteControl',
-			       display : $('#pre_AndroidRemoteControl'),
-			       search : $('#in_AndroidRemoteControlLogSearch'),
-			       control : $('#bt_AndroidRemoteControlLogStopStart'),
+			       log : 'AndroidTV',
+			       display : $('#pre_AndroidTV'),
+			       search : $('#in_AndroidTVLogSearch'),
+			       control : $('#bt_AndroidTVLogStopStart'),
            		});
 		}
 	});
