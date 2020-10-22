28ae0e05a779a94ba314048771171ae6af66a93d
Halbout Michael <michaelhalbout@gmail.com>
2020-10-22 12:01:45 +0200

    Update AndroidRemoteControlCmd.class.php

--- core/class/AndroidRemoteControlCmd.class.php
+++ core/class/AndroidRemoteControlCmd.class.php
@@ -1,13 +1,13 @@
 <?php
 require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
 
-class AndroidRemoteControlCmd extends cmd
+class AndroidTVCmd extends cmd
 {
 
 public function execute($_options = null)
 {
 $ARC = $this->getEqLogic();
-$ARC->checkAndroidRemoteControlStatus();
+$ARC->checkAndroidTVStatus();
 
 $sudo = exec("\$EUID");
 if ($sudo != "0") {
@@ -15,7 +15,7 @@ $sudo_prefix = "sudo ";
 }
 $ip_address = $ARC->getConfiguration('ip_address');
 
-log::add('AndroidRemoteControl', 'info', 'Command ' . $this->getConfiguration('commande') . ' sent to android device at ip address : ' . $ip_address);
+log::add('AndroidTV', 'info', 'Command ' . $this->getConfiguration('commande') . ' sent to android device at ip address : ' . $ip_address);
 shell_exec($sudo_prefix . "adb -s ".$ip_address.":5555 " . $this->getConfiguration('commande'));
 
 if (stristr($this->getLogicalId(), 'setVolume')){
