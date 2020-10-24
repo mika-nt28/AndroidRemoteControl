147b7a0ed1334a524d1b6da9058621213891c32e
Halbout Michael <michaelhalbout@gmail.com>
2020-10-24 13:59:55 +0200

    Update install.sh

--- 3rdparty/install.sh
+++ 3rdparty/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-touch /tmp/AndroidRemoteControl_dep
+touch /tmp/AndroidTV_dep
 if [[ $EUID -ne 0 ]]; then
   sudo_prefix=sudo;
 fi
-echo 0 > /tmp/AndroidRemoteControl_dep
+echo 0 > /tmp/AndroidTV_dep
 echo "############################################################################"
 echo "# Installation in progress"
 echo "############################################################################"
@@ -11,24 +11,24 @@ echo "##########################################################################
 echo "# Update repository packages and install dependencies"
 echo "############################################################################"
 
-echo 5 > /tmp/AndroidRemoteControl_dep
+echo 5 > /tmp/AndroidTV_dep
 $sudo_prefix apt-get -y update
-echo 50 > /tmp/AndroidRemoteControl_dep
+echo 50 > /tmp/AndroidTV_dep
 
 $sudo_prefix apt-get -y install android-tools-adb
 
-echo 75 > /tmp/AndroidRemoteControl_dep
+echo 75 > /tmp/AndroidTV_dep
 $sudoPrefix adb start-server
 
-echo 90 > /tmp/AndroidRemoteControl_dep
+echo 90 > /tmp/AndroidTV_dep
 
 echo "############################################################################"
 echo "# Installation Information"
 echo "############################################################################"
 $sudo_prefix cat /etc/os-release
 
-echo 100 > /tmp/AndroidRemoteControl_dep
-rm /tmp/AndroidRemoteControl_dep
+echo 100 > /tmp/AndroidTV_dep
+rm /tmp/AndroidTV_dep
 echo "############################################################################"
 echo "# Installation finished"
 echo "############################################################################"
