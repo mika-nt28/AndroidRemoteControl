2efd145bf8e320c599e2fa5a77274aae4e1aa76f
Halbout Michael <michaelhalbout@gmail.com>
2020-12-30 06:15:51 +0100

    Update AndroidTVCmd.class.php

--- core/class/AndroidTVCmd.class.php
+++ core/class/AndroidTVCmd.class.php
@@ -1,28 +1,19 @@
 <?php
 require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
 
-class AndroidTVCmd extends cmd
-{
+class AndroidTVCmd extends cmd{
+  public function execute($_options = null){
+    $ARC = $this->getEqLogic();
+    $ARC->checkAndroidTVStatus();
 
-public function execute($_options = null)
-{
-$ARC = $this->getEqLogic();
-$ARC->checkAndroidTVStatus();
-
-$sudo = exec("\$EUID");
-if ($sudo != "0") {
-$sudo_prefix = "sudo ";
-}
-$ip_address = $ARC->getConfiguration('ip_address');
-
-log::add('AndroidTV', 'info', 'Command ' . $this->getConfiguration('commande') . ' sent to android device at ip address : ' . $ip_address);
-shell_exec($sudo_prefix . "adb -s ".$ip_address.":5555 " . $this->getConfiguration('commande'));
-
-if (stristr($this->getLogicalId(), 'setVolume')){
-shell_exec($sudo_prefix . "adb -s ".$ip_address.":5555 shell media volume --stream 3  --set " . $_options['slider']);
-}
-
-$ARC->updateInfo();
-}
-
+    $sudo = exec("\$EUID");
+    if ($sudo != "0")
+      $sudo_prefix = "sudo ";
+    $ip_address = $ARC->getConfiguration('ip_address');
+    log::add('AndroidTV', 'info', 'Command ' . $this->getConfiguration('commande') . ' sent to android device at ip address : ' . $ip_address);
+    shell_exec($sudo_prefix . "adb -s ".$ip_address.":5555 " . $this->getConfiguration('commande'));
+    if (stristr($this->getLogicalId(), 'setVolume'))
+     shell_exec($sudo_prefix . "adb -s ".$ip_address.":5555 shell media volume --stream 3  --set " . $_options['slider']);
+    $ARC->updateInfo();
+    }
 }
